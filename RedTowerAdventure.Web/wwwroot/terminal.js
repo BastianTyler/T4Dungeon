c6bc48a968ee4663321257e5719025c1c1b2bbba
BastianTyler <66173323+BastianTyler@users.noreply.github.com>
2026-04-24 16:51:07 -0700

    Got rid of template files and added structure to display terminal

---
+++ RedTowerAdventure.Web/wwwroot/terminal.js
@@ -0,0 +1,36 @@
+﻿window.terminalInterop = {
+    term: null,
+    fitAddon: null,
+
+    init: function (dotnetRef) {
+        this.term = new Terminal({
+            cursorBlink: true,
+            fontSize: 16,
+            fontFamily: 'Cascadia Code, Consolas, monospace',
+            theme: {
+                background: '#0a1628',
+                foreground: '#00ff41',
+                cursor: '#00ff41',
+            },
+            cols: 80,
+            rows: 24,
+        });
+
+        this.fitAddon = new FitAddon.FitAddon();
+        this.term.loadAddon(this.fitAddon);
+        this.term.open(document.getElementById('terminal-container'));
+        this.fitAddon.fit();
+
+        this.term.onData(data => {
+            dotnetRef.invokeMethodAsync('OnTerminalInput', data);
+        });
+    },
+
+    write: function (text) {
+        if (this.term) this.term.write(text);
+    },
+
+    writeLine: function (text) {
+        if (this.term) this.term.write(text + '\r\n');
+    }
+};
