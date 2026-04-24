71e47e10e9b8d277ae06d6556db7bc40e2cf6a08
BastianTyler <66173323+BastianTyler@users.noreply.github.com>
2026-04-24 16:17:57 -0700

    Trying to get it to open on both web and local

---
+++ output/wwwroot/terminal.js
@@ -0,0 +1,24 @@
+﻿window.term = null;
+
+window.setupTerminal = (elementId) => {
+    const container = document.getElementById(elementId);
+
+    if (!container) {
+        console.error("Terminal container not found");
+        return;
+    }
+
+    // Prevent double initialization
+    if (window.term) {
+        return;
+    }
+
+    window.term = new Terminal();
+    window.term.open(container);
+};
+
+window.writeToTerminal = (text) => {
+    if (window.term) {
+        window.term.writeln(text);
+    }
+};
